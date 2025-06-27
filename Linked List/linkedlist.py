768260247c082267488206102ade13d925098d68
kaushalsatam <kaushalabd17@gmail.com>
2025-06-27 23:25:50 +0530

    printing Linked List and pushing and popping nodes in the Linked List

--- Linked List/linkedlist.py
+++ Linked List/linkedlist.py
@@ -4,11 +4,65 @@ class Node:
         self.next = None
 
 class LinkedList:
+    # Linked List constructor
     def __init__(self, value) :
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
 
+    # method to print Linked List
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+
+            self.length -= 1
+            return temp.value
+        else:
+            temp = self.head
+            pre = self.head
+            while temp.next is not None:
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            pre.next = None
+            self.length -= 1
+            return temp.value
+
+
 my_linked_list = LinkedList(11)
-print(my_linked_list.head.value)
+my_linked_list.append(12)
+my_linked_list.append(13)
+my_linked_list.append(14)
+my_linked_list.append(15)
+print("Linked List before Popping")
+my_linked_list.print_list()
+print("Linked List after Popping")
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+my_linked_list.print_list()
